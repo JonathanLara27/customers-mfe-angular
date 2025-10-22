043b3e80c939cbf94f91ee617cc258c81fac9030
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2025-10-22 03:17:47 -0500

    Agregar lógica y estructura al componente CustomerList, incluyendo la integración de servicios y la configuración de paginación.

--- projects/customers-mfe/src/app/pages/customer-list/customer-list.component.ts
+++ projects/customers-mfe/src/app/pages/customer-list/customer-list.component.ts
@@ -1,14 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewChild } from '@angular/core';
+import { CustomerService } from '../../core/services/customer.service';
+import { TableComponent } from '../../shared/table/table.component';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
   imports: [
     CommonModule,
+    TableComponent,
+    MatTableModule,
+    MatPaginatorModule,
   ],
+  providers: [CustomerService],
   templateUrl: './customer-list.component.html',
-  styleUrl: './customer-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./customer-list.component.scss'],
 })
-export class CustomerListComponent { }
+export class CustomerListComponent { 
+
+  private readonly customerService = inject(CustomerService);
+
+
+  public customers = this.customerService.customers;
+  public total = this.customerService.total;
+  public isLoading = this.customerService.isLoading;
+  public error = this.customerService.error;
+
+  public page = this.customerService.page;
+  public pageSize = this.customerService.pageSize;
+
+  public columnsCustomers = this.customerService.columnsCustomers;
+  public actionsCustomers = this.customerService.actionsCustomers;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(TableComponent) tableComponent!: TableComponent;
+
+  ngAfterViewInit(): void {
+    this.customerService.setPaginator(this.paginator);
+    this.customerService.setTable(this.tableComponent);
+    this.customerService.valueChangesPaginator();
+  }
+  
+
+}
