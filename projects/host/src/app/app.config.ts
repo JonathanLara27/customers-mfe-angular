f65a3f007808ac693a9ef322482a211f304bda6c
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2025-10-22 03:14:44 -0500

    Agregar interceptor HTTP y configurar HttpClient en la aplicación

--- projects/host/src/app/app.config.ts
+++ projects/host/src/app/app.config.ts
@@ -1,13 +1,21 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { httpInterceptor } from '../core/interceptors/http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    ]
+    // Proveer HttpClient en el host para que remotos standalone puedan inyectarlo
+    provideHttpClient(
+      withInterceptors([
+        httpInterceptor
+      ])
+    ),
+  ]
 };
