e1ad4f4b2aa50d2f1fd226ebf3e132f0f757bbd8
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2025-10-22 03:16:42 -0500

    Agregar servicio ApiCustomerService y lógica para gestión de clientes

--- projects/customers-mfe/src/app/core/services/api-customer.service.ts
+++ projects/customers-mfe/src/app/core/services/api-customer.service.ts
@@ -1,10 +1,43 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ENDPOINT_CUSTOMERS } from '../../shared/constants';
+import { map, Observable } from 'rxjs';
+import { CreateCustomerDTO, Customer, CustomerResponse } from '../../shared/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiCustomerService {
 
+  private readonly http = inject(HttpClient);
+  private baseUrl = ENDPOINT_CUSTOMERS;
+
   constructor() { }
+
+
+  getCustomers(page: number, limit: number): Observable<CustomerResponse> {
+    let params = new HttpParams()
+      .set('_page', String(page))
+      .set('_limit', String(limit));
+    return this.http.get<Customer[]>(`${this.baseUrl}`, { observe: 'response', params })
+      .pipe(
+        map(resp => {
+          const total = Number(resp.headers.get('X-Total-Count') ?? resp.body?.length ?? 0);
+          return { data: resp.body ?? [], total };
+        })
+      );
+  }
+
+  getCustomerById(id: number) {
+    return this.http.get<Customer>(`${this.baseUrl}/${id}`);
+  }
+
+  createCustomer(data: CreateCustomerDTO) {
+    return this.http.post<Customer>(`${this.baseUrl}`, data);
+  }
+
+  patchCustomer(id: number, data: Partial<CreateCustomerDTO>) {
+    return this.http.patch<Customer>(`${this.baseUrl}/${id}`, data);
+  }
 
 }
--- projects/customers-mfe/src/app/core/services/customer.service.ts
+++ projects/customers-mfe/src/app/core/services/customer.service.ts
@@ -1,8 +1,98 @@
-import { Injectable } from '@angular/core';
-
+import { computed, inject, Injectable, resource, signal } from '@angular/core';
+import { ApiCustomerService } from './api-customer.service';
+import { firstValueFrom } from 'rxjs';
+import { TableComponent } from '../../shared/table/table.component';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { FormBuilder } from '@angular/forms';
+import { COLUMNS_CUSTOMERS } from '../../shared/constants';
+import { ButtonColumn } from '../../shared/table/table.interface';
 @Injectable()
 export class CustomerService {
 
-  constructor() { }
+  private readonly apiCustomerService = inject(ApiCustomerService);
+
+  private fb = inject(FormBuilder);
+  private statePage = signal<{ pageIndex: number; pageSize: number }>({ pageIndex: 0, pageSize: 5 });
+  
+  private customerResource = resource({
+    loader: async () => await firstValueFrom(this.apiCustomerService.getCustomers(this.statePage().pageIndex, this.statePage().pageSize)),
+  })
+
+
+  private tableComponent: TableComponent | null = null;
+  private paginator: MatPaginator | null = null;
+  
+  constructor() { 
+    this.valueChangesSearchForm();
+  }
+  public customers = computed(() => this.customerResource.value()?.data ?? []);
+  public total = computed(() => this.customerResource.value()?.total);
+  public isLoading = computed(() => this.customerResource.isLoading());
+  public error = computed(() => this.customerResource.error());
+  public page = computed(() => this.statePage().pageIndex);
+  public pageSize = computed(() => this.statePage().pageSize);
+
+  public columnsCustomers = computed(() => COLUMNS_CUSTOMERS);
+
+  public actionsCustomers = computed<ButtonColumn>(() => ({
+    field: 'actions',
+    header: 'Acciones',
+    buttons: [
+      {
+        action: (row: any) => {
+          console.log('Editar cliente:', row);
+        },
+        label: (row: any) => 'Editar',
+        icon: (row: any) => 'edit',
+        show: (row: any) => true,
+      },
+      {
+        action: (row: any) => {
+          console.log('Eliminar cliente:', row);
+        },
+        label: (row: any) => 'Eliminar',
+        icon: (row: any) => 'delete',
+        buttonClass: 'mat-warn',
+        show: (row: any) => true,
+      }
+    ]
+  }));
+
+
+  public form = this.fb.nonNullable.group({
+    search: [''],
+  });
+
+  public setPaginator(paginator: MatPaginator): void {
+    this.paginator = paginator;
+  }
+
+  public setTable(tableComponent: TableComponent): void {
+    this.tableComponent = tableComponent;
+  }
+
+  public valueChangesPaginator() {
+    if(!this.paginator) return;
+    this.paginator.page.subscribe({
+      next: (event: PageEvent) => {
+        console.log("🚀 ~ CustomerService ~ valueChangesPaginator ~ event:", event)
+        const page = (event.pageIndex  || 0) + 1;
+        const pageSize = event.pageSize || 10;
+        this.statePage.set({ pageIndex: page, pageSize });
+        this.customerResource.reload();
+      }
+    });
+  }
+
+  private valueChangesSearchForm() {
+    this.form.get('search')?.valueChanges.subscribe({
+      next: (value) => {
+        if(!value || !this.tableComponent) return;
+        const normalizeValue = value.trim().toLowerCase();
+        this.tableComponent.setFilter(normalizeValue);
+      }
+    });
+  }
+  
 
 }
