31663bb70ddccfb544930806cfc37d78a046cfe3
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2025-10-23 11:27:25 -0500

    Refactor customer list component to improve UI and loading state handling

--- projects/customers-mfe/src/app/pages/customer-list/customer-list.component.ts
+++ projects/customers-mfe/src/app/pages/customer-list/customer-list.component.ts
@@ -1,32 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, ViewChild } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { CustomerService } from '../../core/services/customer.service';
-import { TableComponent } from '../../shared/table/table.component';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderPageComponent } from '../../shared/headerPage/headerPage.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
   imports: [
     CommonModule,
-    TableComponent,
     MatTableModule,
     MatPaginatorModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatProgressBarModule,
+    HeaderPageComponent,
   ],
   providers: [CustomerService],
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.scss'],
 })
-export class CustomerListComponent { 
-
+export default class CustomerListComponent { 
+  
   private readonly customerService = inject(CustomerService);
 
+  public title = this.customerService.title;
+  public subtitle = this.customerService.subtitle;
 
   public customers = this.customerService.customers;
   public total = this.customerService.total;
   public isLoading = this.customerService.isLoading;
   public error = this.customerService.error;
+
+  public form = this.customerService.form;
 
   public page = this.customerService.page;
   public pageSize = this.customerService.pageSize;
@@ -35,12 +47,14 @@ export class CustomerListComponent {
   public actionsCustomers = this.customerService.actionsCustomers;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(TableComponent) tableComponent!: TableComponent;
 
   ngAfterViewInit(): void {
     this.customerService.setPaginator(this.paginator);
-    this.customerService.setTable(this.tableComponent);
     this.customerService.valueChangesPaginator();
+  }
+
+  public navigateToTableView() {
+    return this.customerService.navigateToTable()
   }
   
 
