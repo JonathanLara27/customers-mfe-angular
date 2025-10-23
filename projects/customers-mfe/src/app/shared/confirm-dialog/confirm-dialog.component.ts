97bdf5433eb23903d30d81856a4c35b89e2ed70b
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2025-10-23 11:28:00 -0500

    Refactor confirm dialog component with new HTML structure, styles, and signal-based data handling

--- projects/customers-mfe/src/app/shared/confirm-dialog/confirm-dialog.component.ts
+++ projects/customers-mfe/src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -1,14 +1,60 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+export interface DialogDataConfirm {
+  title: string;
+  message: string;
+  titleButtonMain?: string,
+  tittleButtonSecondary?: string
+}
 
 @Component({
   selector: 'app-confirm-dialog',
   standalone: true,
   imports: [
     CommonModule,
+    MatDialogModule,
   ],
   templateUrl: './confirm-dialog.component.html',
   styleUrl: './confirm-dialog.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfirmDialogComponent { }
+export class ConfirmDialogComponent { 
+
+  public title = signal<string>('');
+
+  public message = signal<string>('');
+
+  public titleButtonMain = signal<string>('Aceptar');
+  public tittleButtonSecondary = signal<string>('Cancelar');
+
+  private dialogRef = inject(MatDialogRef<ConfirmDialogComponent>);
+
+  public data: DialogDataConfirm = inject(MAT_DIALOG_DATA);
+
+
+  ngOnInit(): void {
+    this.initializeData();
+  }
+
+  private initializeData() {
+    this.title.set(this.data?.title);
+    this.message.set(this.data?.message);
+    if (this.data?.titleButtonMain) {
+      this.titleButtonMain.set(this.data?.titleButtonMain);
+    }
+    if (this.data?.tittleButtonSecondary) {
+      this.tittleButtonSecondary.set(this.data?.tittleButtonSecondary);
+    }
+  }
+
+  public close(){
+    this.dialogRef.close(false);
+  }
+
+  public confirm(){
+    this.dialogRef.close(true);
+  }
+
+}
